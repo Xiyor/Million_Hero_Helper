751f68a222fc50882fb38dddc178d4028a0b73b8
Xiyor <yaoxu_seu@126.com>
2018-01-28 23:07:11 +0800

    [main.py]: 修改错误模块名

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import time
 time_s = time.time()
 
 #coding:utf-8
-from answer_helper.ocr_api import baidu_ocr_api
-from answer_helper.simulator_screenshot import screenshot_obtain
-from answer_helper.start_web_search import start_search
+from Million_Hero_Helper.ocr_api import baidu_ocr_api
+from Million_Hero_Helper.simulator_screenshot import screenshot_obtain
+from Million_Hero_Helper.start_web_search import start_search
 
 app_label = "Qt5QWindowIcon"
 screenshot_obtain(app_label)
